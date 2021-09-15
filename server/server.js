756c1fde7fe2f5d73c8d4d809c3c52b0c0c3f2b2
Jun Hwang <jhwang2525@gmail.com>
2021-09-14 21:47:19 -0700

    express

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express()
+
+
+// production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+// app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+  db.on("error", (err) => {
+    console.error("MongoDB connection error: ", err);
+  });
