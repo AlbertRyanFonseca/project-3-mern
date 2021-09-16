1dbe8aebae0753e317fe5b8dd8539a6b263e17a6
Joseph <joe@uhhuhohyeah.com>
2021-09-16 12:36:40 -0700

    added basic framework for the Post component along with css file

---
+++ client/src/components/Post/Post.js
@@ -0,0 +1,12 @@
+import './Post.css';
+import React from "react";
+
+function Post() {
+    return (
+        <div>
+        <h1>Supply Post</h1>    
+        </div>
+    );
+};
+
+export default Post;
