d625c51a70d97c2fe6c7def1535ed3be2110932a
Joseph <joe@uhhuhohyeah.com>
2021-09-16 17:34:54 -0700

    added text area to post component

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -4,7 +4,21 @@ import React from "react";
 function Post() {
     return (
         <div>
-        <h1>Supply Post</h1>    
+        <h1>Supply Post</h1> 
+        <form>
+
+        <textarea
+                placeholder="New post..."
+                value={postText}
+                className=""
+                onChange={handleChange}
+            ></textarea>
+        <button className="" type="submit">
+          Post
+        </button>
+
+        </form>
+
         </div>
     );
 };
