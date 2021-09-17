36d2cf1d14bb340a58b7aaa47b697d4d3d820cbf
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-16 19:25:41 -0700

    Added HomePage and Icon compomonents, made adjustments to index.js and App.js

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import './HomePage.css';
+
+function HomePage(props) {
+    return (
+        <div className="icons__container">
+            <h2>Hello I'm coding.</h2>
+        </div>
+    );
+
+};
+
+export default HomePage;
---
+++ client/src/components/Icon/Icon.js
@@ -0,0 +1,11 @@
+import './Icon.css';
+
+function Icon(props) {
+    return (
+        <div className="icon__containter">
+
+        </div>
+    );
+};
+
+export default Icon;
