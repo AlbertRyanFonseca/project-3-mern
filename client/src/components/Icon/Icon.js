21fe2e46c2ea1b1ec949e37638ce525cfab94a40
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-21 12:32:22 -0700

    Added some type of functionality meant for the HomePage component. Ignore the Icon.js file as it will get cleaned with some fucntionality and CSS. It will be less complex as well

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -3,7 +3,24 @@ import './Icon.css';
 function Icon(props) {
     return (
         <div className="icon__containter">
-            <span className="cirle"></span>
+            <div class="row">
+                <div class="column">
+                    <span class="circle">
+                        <span class="inner-circle">
+                            <h2 class="circle-title">PI</h2>
+                            <p class="circle-desc">Paint Items</p>
+                        </span>
+                    </span>
+                </div>
+                <div class="column">
+                    <span class="circle">
+                        <span class="inner-circle">
+                            <h2 class="circle-title">PI</h2>
+                            <p class="circle-desc">Paint Items</p>
+                        </span>
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
