a953cdfe5967332e8400efff7ebd6a7b3d146416
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-21 21:13:50 -0700

    Added class names and styling, server.js will be fixed after pull

--- server/server.js
+++ server/server.js
@@ -7,28 +7,26 @@ const mongoose = require("mongoose");
 // const { typeDefs, resolvers } = require("./schemas");
 const PORT = process.env.PORT || 3001;
 
-/*
 
-production, serve client/build as static assets
+
+// production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-  
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-*/
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // app.use(routes);
 
 db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      // log where we can go to test our GQL API
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     //  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
   });
-  db.on("error", (err) => {
-    console.error("MongoDB connection error: ", err);
-  });
-  
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
+});
