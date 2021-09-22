aeb3d4be715e7c30d5fd1473a3b3ef726b3f7369
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-21 20:26:30 -0700

    Changed class to className. Deleted information from the spans

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -3,20 +3,18 @@ import './Icon.css';
 function Icon(props) {
     return (
         <div className="icon__containter">
-            <div class="row">
-                <div class="column">
-                    <span class="circle">
-                        <span class="inner-circle">
-                            <h2 class="circle-title">PI</h2>
-                            <p class="circle-desc">Paint Items</p>
+            <div className="row">
+                <div className="column">
+                    <span className="circle">
+                        <span className="inner-circle">
+
                         </span>
                     </span>
                 </div>
-                <div class="column">
-                    <span class="circle">
-                        <span class="inner-circle">
-                            <h2 class="circle-title">PI</h2>
-                            <p class="circle-desc">Paint Items</p>
+                <div className="column">
+                    <span className="circle">
+                        <span className="inner-circle">
+
                         </span>
                     </span>
                 </div>
