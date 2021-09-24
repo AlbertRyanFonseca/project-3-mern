143d6ae07b2efe27ae9235d52b9ca39637040dca
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-23 20:59:11 -0700

    Added components and correctly handled data. Need some styling fixing.

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -3,20 +3,9 @@ import './Icon.css';
 function Icon(props) {
     return (
         <div className="icon__containter">
-            <div className="row">
-                <div className="column">
-                    <span className="outer__circle">
-                        <span className="inner__circle">
-
-                        </span>
-                    </span>
-                </div>
-                <div className="column">
-                    <span className="outer__circle">
-                        <span className="inner__circle">
-
-                        </span>
-                    </span>
+            <div className="outer__circle">
+                <div className="inner__circle">
+                    {props.children}
                 </div>
             </div>
         </div>
