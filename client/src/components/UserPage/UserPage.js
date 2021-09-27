0a58a04963b9b869e706bd6a8215c74a5f8da17b
jhwang2525 <80090287+jhwang2525@users.noreply.github.com>
2021-09-27 01:21:20 -0700

    Userpage update

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -1,15 +1,45 @@
-import './UserPage.css'
+import './UserPage';
+import './UserPage.css';
+import React from "react";
+import Header from "header";
 
-function UserPage(props) {
+
+
+function UserPage() {
+    var user = JSON.parse(localStorage.getItem('user'))
     return (
-        <div className="icons__container">
-            <div className="icon__title">
-                <h2>{props.title}Hello!</h2>
-                <div className="icon__description">{props.description}</div>
+        <div className="row">
+        <div className="col-md-9">
+          <div className="card bg-light">
+            <div className="card-body">
+                  <h1 className="card-title" title="user-page"><span id="greeting">Welcome, {user.firstname1}!</span></h1>
+                  <hr></hr>
+                  <Header headers={UserPage} />
+                  <ul className="list">
+        <li className="Posts">
+          <Link
+            className="link"
+            to={`/@${this.props.UserPage.username}`}>
+            My Posts
+          </Link>
+        </li>
+        <li className="Interest">
+          <Link
+            className="link"
+            to={`/@${this.props.UserPage.username}/interest`}>
+            Interest
+          </Link>
+                         </li>
+                     </ul>
+                </div>
             </div>
         </div>
-    );
+    </div>
 
-};
+
+
+
+    );
+}; 
 
 export default UserPage;
