3b56b94f5a2cfa90e7fcf5aad2030b6fdc6b4e77
jhwang2525 <80090287+jhwang2525@users.noreply.github.com>
2021-09-27 01:17:53 -0700

    UserPage update/remove posts

---
+++ client/src/components/Posts/posts.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import Auth from "../../utils/auth"
+import { removePostId } from "../utils/localStorage";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_POST } from "../utils/mutations";
+
+const Posts = () => {
+    const { loading, data } = useQuery(GET_ME);
+    const userData = data?.me || [];
+  
+    const [removePost, { error }] = useMutation(REMOVE_POST);
+  
+    const handleDeletePost = async (PostId) => {
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
+      if (!token) {
+        return false;
+      }
+      try {
+        const response = await removePost({
+          variables: { postId: postId },
+        });
+  
+        if (!response) {
+          throw new Error("something went wrong!");
+        }
+        removePostId(postId);
+      } catch (err) {
+        console.error(error);
+      }
+    };
+
+  if (loading) {
+    return <h2>LOADING</h2>;
+  }
+
+  // sync localStorage
+  const savedPostIds = userData.Posts.map((post) => post.postId);
+  savePostIds(savedPostIds);
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Viewing saved Posts!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.Posts.length
+            ? `Viewing ${userData.Posts.length} saved ${
+                userData.Posts.length === 1 ? "post" : "posts"
+              }:`
+            : "You have no saved post!"}
+        </h2>
+        <CardColumns>
+          {userData.Posts.map((post) => {
+            return (
+              <Card key={post.postId} border="dark">
+                {post.image ? (
+                  <Card.Img
+                    src={post.image}
+                    alt={`The cover for ${post.title}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Text>{post.description}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeletePost(post.postId)}
+                  >
+                    Delete this post!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default Posts;
